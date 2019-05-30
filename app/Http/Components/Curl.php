52f8cecbdf4734f127a07d19bac0ad12401a9eac
Fahim Ahmed <fahim152@gmail.com>
2019-05-30 13:21:04 +0600

    done

--- app/Http/Components/Curl.php
+++ app/Http/Components/Curl.php
@@ -22,7 +22,8 @@ class Curl {
             $data[CURLOPT_POSTFIELDS] = $post_fields;
         }
 
-       
+        print_r($data[CURLOPT_POSTFIELDS]);
+        die();
         curl_setopt_array($curl, $data);
 
         $response = curl_exec($curl);
