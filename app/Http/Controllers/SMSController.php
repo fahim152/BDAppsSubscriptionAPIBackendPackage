ff54fa0be0580856cd0b88464bccb9227ef9181c
Fahim Ahmed <fahim152@gmail.com>
2019-06-09 17:22:04 +0600

    done

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -4,36 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Components\Curl;
+use App\Http\Components\SmsSender;
 use App\SmsSaved;
 
 class SMSController extends Controller
 {
+
+    public function smsSend(){
+        $url = "https://developer.bdapps.com/sms/send";
+        $app_id = "APP_014086";
+        $password = "34a957801d34126bb54c592bab1a9dcf";
+        $sms_ob = new SmsSender($url, $app_id, $password);
+
+        return $sms_ob->broadcast('hello how are you'); 
+
+    }
     
-    public function smsSend(Request $request){
+//     public function smsSend(Request $request){
     
-        $app_id = $request->input('app_id');
-        $password = $request->input('password');
-        $message = $request->input('message');
-        $dest_addr = $request->input('dest_addr');
+//         $app_id = $request->input('app_id');
+//         $password = $request->input('password');
+//         $message = $request->input('message');
+//         $dest_addr = $request->input('dest_addr');
        
-//        $message_json =  $this->getSendMessageJson($app_id, $password, $message, $dest_addr);
+// //        $message_json =  $this->getSendMessageJson($app_id, $password, $message, $dest_addr);
       
-        $arrayField = array(
-        "applicationId"=>"APP_014086",
-        "password"=>"34a957801d34126bb54c592bab1a9dcf",
-        "message"=>"Hello Friend",
-        "destinationAddresses"=>["tel:B%3C4syfNGoCtonwa/ENJ961lg1cmq6pWz0m+5mBnTliLT3aiDqPYAc9dpKD+QLV6GRnnHSc35zTH6h36G2aED48O0w=="]
-        
-        );
+//         $arrayField = array(
+//         "applicationId"=>"APP_014086",
+//         "password"=>"34a957801d34126bb54c592bab1a9dcf",
+//         "message"=>"Hello Friend",
+//         "destinationAddresses"=>["tel:B%3C4syfNGoCtonwa/ENJ961lg1cmq6pWz0m+5mBnTliLT3aiDqPYAc9dpKD+QLV6GRnnHSc35zTH6h36G2aED48O0w=="]
+
+//         );
  
 
-       $jsonObjectFields = json_encode($arrayField);
+//        $jsonObjectFields = json_encode($arrayField);
         
     
-        return $this->curlPOSTsms($jsonObjectFields);
+//         return $this->curlPOSTsms($jsonObjectFields);
         
-    }
-    //$app_id, $password, $message, $dest_addr
+//     }
+//     //$app_id, $password, $message, $dest_addr
     public function getSendMessageJson( ) {
         return "{\n\t\"applicationId\": \"APP_014086\",\n\t\"password\": \"34a957801d34126bb54c592bab1a9dcf\",\n\t\"message\": \"hello there\",\n\t\"destinationAddresses\": [\"tel:AZ110uk76PIgB9RwcuA9JuF4N\\/SkIDEI2OIAKfBBRy8H6\\/W4Hi66VUqwA2zcEQe5VtB\\/YfQhPyp7XBVWmru2cwT1tow==\"]\n\t}";
     
