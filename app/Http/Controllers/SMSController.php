5a7abe8b79fe0fa83afcfe0b5949936a5051dc21
fahim <fahim152@gmail.com>
2019-06-09 17:27:58 +0600

    done

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -16,7 +16,7 @@ class SMSController extends Controller
         $app_id = "APP_014086";
         $password = "34a957801d34126bb54c592bab1a9dcf";
         $sms_ob = new SmsSender($url, $app_id, $password);
-        dd($sms_ob);
+       
         return $sms_ob->broadcast('hello how are you'); 
 
     }
