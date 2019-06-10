145d2abff365e4bbdcd68e7f8d9e27e5ce6d472c
Fahim Ahmed <fahim152@gmail.com>
2019-06-10 12:55:52 +0600

    dpone

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -13,12 +13,14 @@ class SMSController extends Controller
     public function smsSend(Request $request){
         
         $url = "https://developer.bdapps.com/sms/send";
-        $app_id = "APP_014086";
+        $app_id =  $request->input('app_id');
         $message = $request->input('message');
-        $password = "34a957801d34126bb54c592bab1a9dcf";
-        
+        $password = $request->input('password');
+        $is_specific = $request->input('is_specific');
+
         $sms_ob = new SmsSender($url, $app_id, $password);
         
+
         $response =  $sms_ob->broadcast($message);
         
         return $response;
@@ -48,7 +50,7 @@ class SMSController extends Controller
 //         return $this->curlPOSTsms($jsonObjectFields);
         
 //     }
-//     //$app_id, $password, $message, $dest_addr
+
     public function getSendMessageJson( ) {
         return "{\n\t\"applicationId\": \"APP_014086\",\n\t\"password\": \"34a957801d34126bb54c592bab1a9dcf\",\n\t\"message\": \"hello there\",\n\t\"destinationAddresses\": [\"tel:AZ110uk76PIgB9RwcuA9JuF4N\\/SkIDEI2OIAKfBBRy8H6\\/W4Hi66VUqwA2zcEQe5VtB\\/YfQhPyp7XBVWmru2cwT1tow==\"]\n\t}";
     
@@ -84,7 +86,7 @@ class SMSController extends Controller
         $status =  $request->input('status');		
         $frequency =  $request->input('frequency');	
         $timeStamp =  $request->input('timeStamp');			
-
+        $inspect = $request->input($request);	
         $sms = new SmsSaved();
 
         $sms->version = isset($version) ? $version : "";
