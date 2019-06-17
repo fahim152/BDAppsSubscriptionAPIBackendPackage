5ecfdd6cde0cd360681cd74b72b4ce0f4419c6c1
Fahim Ahmed <fahim152@gmail.com>
2019-06-17 16:11:05 +0600

    done sending sms

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -55,7 +55,7 @@ class SMSController extends Controller
                 return $data;
             }
         }
-        return $response;
+        return json_encode($response);
     }
     
 //     public function smsSend(Request $request){
