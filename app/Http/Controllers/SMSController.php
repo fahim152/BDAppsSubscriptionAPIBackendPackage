279e4aa56f2c573cecd4aed28de1821788bdc9d3
Fahim Ahmed <fahim152@gmail.com>
2019-06-17 16:07:06 +0600

    done sending sms

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -40,9 +40,10 @@ class SMSController extends Controller
         
         $response =   json_decode($sms_ob->broadcast($message));
         $statusCode = $response->statusCode;
-        dd($statusCode);
-        
+       
         if($statusCode == 'S1000'){
+            dd($statusCode);
+        
             if(!empty($obj)){
                 $obj->is_sent = true;
                 if($obj->save()){
